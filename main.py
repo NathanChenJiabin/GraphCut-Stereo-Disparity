66e7cc62ae17473aa3855511704e3cfc93f18efe
NathanChenJiabin <chen.jiabinx2016@gmail.com>
2018-12-16 23:56:56 +0100

    edit main programme in main.py

--- main.py
+++ main.py
@@ -1,10 +1,81 @@
 import match
+import cv2
+
+MAX_DENOM = int(2 ** 4)
 
 
-def main():
-    print(".....")
-    pass
+def set_fractions(params, K, lambda1, lambda2):
+    minError = float(2 ** 30)
+    for i in range(1, MAX_DENOM + 1):
+        e = 0
+        numK = 0
+        num1 = 0
+        num2 = 0
+        if K > 0:
+            numK = int(i * K + .5)
+            e += abs(numK / (i * K) - 1.0)
+
+        if lambda1 > 0:
+            num1 = int(i * lambda1 + .5)
+            e += abs(num1 / (i * lambda1) - 1.0)
+
+        if lambda2 > 0:
+            num2 = int(i * lambda2 + .5)
+            e += abs(num2 / (i * lambda2) - 1.0)
+
+        if e < minError:
+            minError = e
+            params.denominator = i
+            params.K = numK
+            params.lambda1 = num1
+            params.lambda2 = num2
+    return params
+
+
+def fix_parameters(mch, params, K, lambda_, lambda1, lambda2):
+    if K < 0:
+        mch.SetParameters(params)
+        K = mch.getK()
+    if lambda_ < 0:
+        lambda_ = K / 5
+    if lambda1 < 0:
+        lambda1 = 3 * lambda_
+    if lambda2 < 0:
+        lambda2 = lambda_
+    params = set_fractions(params, K, lambda1, lambda2)
+    mch.SetParameters(params)
+    return mch
+
+
+# main programme
+def main(file1, file2, color, dispMin, dispMax):
+    # load two images
+    imgLeft = cv2.imread(file1)
+    imgRight = cv2.imread(file2)
+
+    # Default parameters
+    K = -1, lambda_ = -1, lambda1 = -1, lambda2 = -1
+    params = match.Parameters(is_L2=True,
+                              denominator=1,
+                              edgeThresh=8,
+                              lambda1=-1,
+                              lambda2=-1,
+                              K=-1,
+                              maxIter=4)
+
+    # create match instance
+    m = match.Match(imgLeft, imgRight, color)
+
+    m.SetDispRange(dispMin, dispMax)
+    m = fix_parameters(m, params, lambda_, lambda1, lambda2)
+    m.kolmogorov_zabih()
+    m.saveDisparity("../disparity.jpg")
 
 
 if __name__ == '__main__':
-    main()
+    filename_left = "../images/imgL.jpg"
+    filename_right = "../images/imgR.jpg"
+    is_color = True
+    disMin = 0
+    disMax = 16
+    main(filename_left, filename_right, is_color, disMin, disMax)
